978e9804154186a2626391138bcde14b4f46d545
VishruthNarayanan <77874913+VishruthNarayanan@users.noreply.github.com>
2021-06-12 19:09:03 +0530

    Add files via upload

---
+++ Template_Bike_Racer_copy_2021_06_11_13_09_54/sketch.js
@@ -0,0 +1,57 @@
+var path,mainCyclist;
+var pathImg,mainRacerImg1,mainRacerImg2;
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+var distance=0;
+
+var select_oppPlayer,pinkCyclists,redCyclists,pinkCyclists
+
+function preload(){
+  pathImg = loadImage("images/Road.png");
+  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadAnimation("images/mainPlayer3.png");
+}
+
+function setup(){
+  
+createCanvas(displayWidth,displayHeight);
+  
+// Moving background
+path=createSprite(displayWidth,displayHeight);
+path.addImage(pathImg);
+path.velocityX = -5;
+
+//creating boy running
+mainCyclist  = createSprite(70,150,20,20);
+mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+mainCyclist.scale=0.07;
+  
+  
+}
+
+function draw() {
+  background(0);
+  
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("Distance: "+ distance,350,30);
+  
+  if(gameState===PLAY){
+  
+    mainCyclist.y = World.mouseY;
+  
+   edges= createEdgeSprites();
+   mainCyclist .collide(edges);
+  
+  //code to reset the background
+  if(path.x < 0 ){
+    path.x = width/2;
+  }
+  camera.position.x = 70;
+  camera.position.y = mouseY
+ }
+}
